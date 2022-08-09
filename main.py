db191bb799a96c0cf7a9011d5c942d75e7d4812c
Frostman101 <79807432+Frostman101@users.noreply.github.com>
2022-08-08 21:59:15 -0500

    jordan/THOUGH-11 Set up index html to associate/display templates, and static folder content.

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/index", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", context={"request":request})
 
 
 @app.get("/")
@@ -9,13 +17,14 @@ def home():
     return {"Hello World"}
 
 @app.get("/metagame")
-def metagame():
+async def metagame():
     return {"some text"}
 
 @app.get("/tournaments")
-def tournaments():
+async def tournaments():
     return {"some text"}
 
 @app.get("/archives")
-def archives():
+async def archives():
     return {"some text"}
+
