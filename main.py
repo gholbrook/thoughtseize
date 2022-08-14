776136ac0bc3017df2bed171ff046b4626af4fcb
Frostman101 <79807432+Frostman101@users.noreply.github.com>
2022-08-14 13:42:08 -0500

    jordan/THOUGH-16 stuck on query parameter. can't get this example to work : /archives/articles?format=modern

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/index", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse("index.html", context={"request":request})
-
-
-@app.get("/")
-def home():
-    return {"Hello World"}
+database = {}
+database2 = {}
+database3 = {}
+for indexnum in range(10):
+    database[f'Modern-Article-{indexnum}'] = dict(Author=f"Reid Duke", Format=f"Modern")
+    database2[f'Standard-Article-{indexnum}'] = dict(Author=f"Ari Lax", Format=f"Standard")
+    database3[f'Legacy-Article-{indexnum}'] = dict(Author=f"Gene Simmons", Format=f"Legacy")
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", context={"request": request})
 
 @app.get("/metagame")
 async def metagame():
@@ -24,7 +26,21 @@ async def metagame():
 async def tournaments():
     return {"some text"}
 
-@app.get("/archives")
-async def archives():
-    return {"some text"}
+@app.get("/archives/articles")
+async def archives(request: Request):
+    return database,database2,database3
 
+@app.get("/archives/articles/modern")
+async def archives(request: Request):
+    return database
+
+@app.get("/archives/articles/standard")
+async def archives(request: Request):
+    return database2
+
+@app.get("/archives/articles/legacy")
+async def archives(request: Request):
+    return database3
+
+@app.get("/archives/articles{format_id}") #ex. want output "/archives/articles?format=modern" to return database
+async def read_item(format_id):
